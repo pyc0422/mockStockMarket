e5341d70d4d91b8d5bc91be062db9399a18d1cea
Rain <pyc0422@gmail.com>
2022-09-09 00:13:17 -0500

    handled buy and sell in db

--- db/controller/Stocks.js
+++ db/controller/Stocks.js
@@ -2,7 +2,7 @@ const db = require('../index');
 
 module.exports = {
   findOne: (symbol, cb) => {
-    let query = `SELECT * from stocks where symbol=?`
+    let query = `SELECT * from stocks where symbol=?`;
     return db.query(query, [symbol], function(err, results) {
       if (err) throw err;
       cb(results);
@@ -11,32 +11,42 @@ module.exports = {
 
   insert: (stockData, cb) => {
     console.log('stockData: ', stockData);
-    let query = `SELECT * from stocks where symbol=?`
+    let query = `SELECT * from stocks where symbol=?`;
     db.query(query, [stockData.symbol], function(err, results) {
       if (err) throw err;
       console.log('controller stocks: ', results);
       if (!results.length) {
         let query = `INSERT INTO stocks (symbol, name, shares, price, total, user_id) VALUES (?, ?, ?, ?, ?, ?)`;
         let params = [stockData.symbol, stockData.name, stockData.shares, stockData.price, stockData.total, stockData.user_id];
-        return db.query(query, params, function(err) {
+        db.query(query, params, function(err) {
           if (err) throw err;
           cb();
-        })
+        });
       } else {
         results = results[0]
-        let newPrice = (results.price + stockData.price) / 2;
-        let newShares = results.shares + stockData.shares;
-        let newTotal = results.total + stockData.total;
-        let query = `UPDATE stocks SET price=?, shares=?, total=? WHERE symbol=?`
-        let params =  [newPrice, newShares, newTotal, stockData.symbol]
-        return db.query(query, params, function(err) {
+        if (stockData.action === 'buy') {
+          let newShares = results.shares + stockData.shares;
+          let newTotal = results.total + stockData.total;
+        } else {
+          let newShares = results.shares - stockData.shares;
+          if (newShares < 0) {
+            cb('not enough shares to sell');
+            return;
+          }
+          let newTotal = results.total - stockData.total;
+        }
+        let newPrice = newTotal / newShares;
+        let query = `UPDATE stocks SET price=?, shares=?, total=? WHERE symbol=?`;
+        let params = [newPrice, newShares, newTotal, stockData.symbol];
+        db.query(query, params, function(err) {
           if (err) throw err;
-            cb();
-        })
-      }
+          cb();
+        });
     })
 
   },
+
+
 
 
   findByUser: (user, cb) => {
@@ -46,4 +56,4 @@ module.exports = {
       cb(results)
     });
   }
-}
+};
